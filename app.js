eec6dff27db405bdb8d912e73e9c5bf4b76b4e15
Kamil <kamilanwar2001@gmail.com>
2021-01-05 16:16:34 +0400

    added objects and movement mech

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ class Player {
         playerImg.style.display="block";
         playerContainer.style.top =`${this.x}px`;
         playerContainer.style.left = `${this.y}px`;
-        console.log(this.h);
     }
 
     update(){
@@ -26,30 +25,87 @@ class Player {
         this.ySpeed += gravity
 
         playerContainer.style.top =`${this.y}px`;
+        
         if(this.y > 750-this.h){
             this.ySpeed = 0;
+            this.xSpeed = 1.8;
+            canJump = true;
             playerImg.classList.add('addAnimation');
+        }else{
+            canJump = false
         }
+         
     }
 }
 
+class Obstacle{
+
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.h = 50;
+        this.w = 50;
+    }
+
+    show(){
+        canvasElement.fillStyle='grey'
+        canvasElement.fillRect(this.x,this.y,this.w,this.h)
+    }
+
+   
+   
+}
+
+var obstacles = [];
+var obstacleXPos = 500
 var p;
 var gravity = 0.1;
+var canJump = false;
+
+
 window.onload= function(){
     start();
     setInterval(update,10)
 };
 
 function start(){
+    //player
     p = new Player(40,40)
     p.show()
+    // obstacle
+    for(i=0;i<10;i++){
+        var newObstacle = new Obstacle(obstacleXPos,750-50)
+        obstacles.push(newObstacle);
+        obstacleXPos+=Math.round(Math.random()*400)+200;
+    }
 }
 
 function update(){
     canvas.width = canvas.width;
 
     canvasElement.fillStyle = 'green'
-    canvasElement.fillRect(0,750,800,50);
+    canvasElement.fillRect(0,750,900,50);
+    // show the player
     p.update()
-   
+    // show the obstacles
+    for(i=0;i<10;i++){
+        obstacles[i].show()
+        obstacles[i].x -= p.xSpeed
+    }
+    
 }
+
+function keyDown(key){
+    console.log(key);
+    if(key.keyCode == 32&&canJump){
+        p.ySpeed -= 7;
+    }
+}
+
+function keyUp(key){
+    if(key.keyCode == 32){
+        p.ySpeed = 0;
+    }
+}
+
+window.onkeydown = keyDown;
